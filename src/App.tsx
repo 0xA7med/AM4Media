3def037ac25080535e54df1e5c8c38db761fd79e
0xA7med <zeero4123@gmail.com>
2025-03-12 22:55:40 +0200

    v4.0.7

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import HomePage from './components/HomePage';
 import AdminPanel from './components/AdminPanel';
+import { useEffect } from 'react';
 import { useVideos } from './videoStore';
 
 function App() {
-  const { loadVideos } = useVideos();
-
-  // Load videos from localStorage when the app starts
-  React.useEffect(() => {
+  const loadVideos = useVideos(state => state.loadVideos);
+  
+  useEffect(() => {
+    console.log("تحميل الفيديوهات عند بدء التطبيق...");
     loadVideos();
   }, [loadVideos]);
 
   return (
-    <Router>
+      <Router>
       <Toaster position="top-center" />
       <Routes>
         <Route path="/" element={<HomePage />} />
